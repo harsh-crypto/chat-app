48da7fd6a115fd9c96d527b7ada58561fe18ba5a
harsh-crypto <54810296+harsh-crypto@users.noreply.github.com>
2022-04-20 03:20:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import './App.css';
 //Using arrow function component
 const App = () => {
   return (
-    // <ChatEngine
-    //   userName='Muganga'
-    //   projectID = '8296ad17-4229-4bcd-a65d-6894718daa54'
-    //   userSecret='5f1d3069-b7dd-413d-865d-60f0dcad04bd'
-
-    // />
+    <ChatEngine
+			userName='harsh'
+			userSecret='harsh123'
+			projectID='4f02cf7c-0e7e-428e-b4a1-a1ff2b446797'
+		/>
     <DirectChat/>
   )
 }
